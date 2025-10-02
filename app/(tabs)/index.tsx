d516aaf4a7efe98d76d3078bcdb7312514c1a12e
Joriko <jori.kosonen@student.lut.fi>
2025-10-02 18:23:26 +0300

    Added possibility to read many receipts, improved flatlist performance

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { extractProducts } from "@/utils/parseTokens";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Image } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
-import { Button, FlatList, StyleSheet, TextInput } from "react-native";
+import { useCallback, useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import MlkitOcr from "react-native-mlkit-ocr";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -12,12 +19,11 @@ export default function HomeScreen() {
   const [image, setImage] = useState<string | null>(null);
   const [data, setData] = useState<any>([]);
   const [total, setTotal] = useState<number | null>(null);
-  const [totalToValidate, setTotalToValidate] = useState<number | null>(null);
+  const [totalToValidate, setTotalToValidate] = useState<number>(0);
+  const [showImage, setShowImage] = useState<boolean>(false);
 
-  const pickImage = async () => {
-    setData(null);
-    let products: { name: string; price: number }[] = [];
-    // No permissions request is necessary for launching the image library
+  const pickImage = async (reset: boolean) => {
+    if (reset) setData([]);
     await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images", "videos"],
       //allowsEditing: true,
@@ -26,6 +32,7 @@ export default function HomeScreen() {
     }).then((result) => {
       setImage(result?.assets?.[0]?.uri || null);
       readProducts(result?.assets?.[0]?.uri || "");
+      setShowImage(true);
     });
   };
 
@@ -50,23 +57,18 @@ export default function HomeScreen() {
         setTotal(products[tIndex].price);
         products.splice(tIndex, 1);
       }
-      let test = 0;
-      products.forEach((element) => {
-        test += element.price;
-      });
-      console.log("test", test);
-      setTotalToValidate(
-        products.reduce((sum, p) => {
-          return sum + p.price;
-        }, 0)
-      );
 
-      setData(products);
+      let total = products.reduce((sum, p) => {
+        return sum + p.price;
+      }, 0);
+      if (totalToValidate > 0) {
+        total += totalToValidate;
+      }
+
+      setTotalToValidate(total);
+      setData((prev: []) => [...(prev || []), ...products]);
+      console.log(products[0]);
     }
-  };
-
-  const readAgain = () => {
-    readProducts(image || "");
   };
 
   // FIXME: input is not letting input commas or points
@@ -91,21 +93,72 @@ export default function HomeScreen() {
     setImage(null);
     setData([]);
     setTotal(null);
-    setTotalToValidate(null);
+    setTotalToValidate(0);
   }
+
+  // FIXME: This is not working properly
+  const removeAtIndex = (index: number) => {
+    const newData = [...data];
+    const removed = newData.splice(index, 1);
+    console.log("removed", removed);
+    setData(newData);
+    setTotalToValidate(
+      newData.reduce((sum, p) => {
+        return sum + p.price;
+      }, 0)
+    );
+  };
+
+  const renderItem = useCallback(
+    ({
+      item,
+      index,
+    }: {
+      item: { name: string; price: number | null };
+      index: number;
+    }) => (
+      <ThemedView style={styles.stepContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Tuotteen nimi"
+          value={item.name}
+          onChangeText={onChangeText(data.indexOf(item), "name")}
+        ></TextInput>
+        <TextInput
+          style={[styles.input, styles.priceInput]}
+          placeholder="Hinta"
+          keyboardType="numeric"
+          value={item.price ? item.price.toString().replace(".", ",") : ""}
+          onChangeText={onChangeText(data.indexOf(item), "price")}
+        ></TextInput>
+        <TouchableOpacity
+          onPress={() => removeAtIndex(index)}
+          style={{ padding: 10 }}
+        >
+          <MaterialIcons name="delete" size={24} color="red" />
+        </TouchableOpacity>
+      </ThemedView>
+    ),
+    []
+  );
 
   return (
     <SafeAreaView
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
       <ThemedView style={styles.container}>
-        {image && (
+        {showImage && image ? (
           <Image
             source={{ uri: image }}
             style={styles.image}
             contentFit="contain"
           />
+        ) : (
+          <ThemedText>
+            {showImage ? "Kuittia ei valittuna" : "Kuva piilotettu"}
+          </ThemedText>
         )}
+
         {image ? (
           <ThemedView
             style={{
@@ -117,12 +170,28 @@ export default function HomeScreen() {
               width: "100%",
             }}
           >
-            <Button title="Lue uudelleen" onPress={readAgain} />
+            <Button
+              title="Lue lisää"
+              onPress={() => {
+                pickImage(false);
+              }}
+            />
+            <Button
+              title={showImage ? "Piilota kuva" : "Näytä kuva"}
+              onPress={() => {
+                setShowImage(!showImage);
+              }}
+            />
             <Button title="Pyyhi tiedot" color="red" onPress={removeData} />
           </ThemedView>
         ) : (
           <ThemedView style={{ marginBottom: 20 }}>
-            <Button title="Valitse kuva kuitista" onPress={pickImage} />
+            <Button
+              title="Valitse kuva kuitista"
+              onPress={() => {
+                pickImage(true);
+              }}
+            />
           </ThemedView>
         )}
         <ThemedText style={{ fontSize: 20, fontWeight: "bold" }}>
@@ -142,25 +211,16 @@ export default function HomeScreen() {
           style={{ width: "100%" }}
           data={data}
           keyExtractor={(_, index) => index.toString()}
-          renderItem={({ item }) => (
-            <ThemedView style={styles.stepContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder="Tuotteen nimi"
-                value={item.name}
-                onChangeText={onChangeText(data.indexOf(item), "name")}
-              ></TextInput>
-              <TextInput
-                style={styles.input}
-                placeholder="Hinta"
-                keyboardType="numeric"
-                value={
-                  item.price ? item.price.toString().replace(".", ",") : ""
-                }
-                onChangeText={onChangeText(data.indexOf(item), "price")}
-              ></TextInput>
-            </ThemedView>
-          )}
+          renderItem={renderItem}
+          getItemLayout={(_, index) => ({
+            length: 60,
+            offset: 60 * index,
+            index,
+          })}
+          removeClippedSubviews={true}
+          maxToRenderPerBatch={10}
+          initialNumToRender={10}
+          windowSize={21}
           ListEmptyComponent={() => (
             <ThemedText>Tuotteita ei luettu vielä</ThemedText>
           )}
@@ -172,9 +232,7 @@ export default function HomeScreen() {
                 onPress={() => setData([...data, { name: "", price: null }])}
               />
               <ThemedView style={{ alignItems: "center", marginTop: 20 }}>
-                <ThemedText>
-                  Laskettu kuitista: {totalToValidate ? totalToValidate : 0}€
-                </ThemedText>
+                <ThemedText>Laskettu kuitista: {totalToValidate}€</ThemedText>
               </ThemedView>
             </>
           )}
@@ -220,5 +278,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#333",
     borderWidth: 1,
     padding: 10,
+    width: "50%",
+  },
+  priceInput: {
+    width: "30%",
   },
 });
